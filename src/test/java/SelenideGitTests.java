262cde4fbdfc6aea683a3c5decfd9825aecdf605
Solo VV <makler63@yandex.ru>
2023-05-22 13:23:12 +0300

    Test find SoftAssertions for JUnit5

---
+++ src/test/java/SelenideGitTests.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideGitTests {
+
+    @Test
+    void findPageSoftAssertions () {
+        Configuration.browserSize = System.getProperty("browserSize","1920x1080");
+        Configuration.browser = System.getProperty("browser", "firefox");
+        open("https://github.com/selenide/selenide");
+        
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $(".filterable-active").shouldHave(text("SoftAssertions"));
+        $(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
