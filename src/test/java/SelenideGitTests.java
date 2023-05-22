134efc9dcd0ee11a5a53c1ce17f1008d69b5fb80
Solo VV <makler63@yandex.ru>
2023-05-22 22:42:51 +0300

    Add configProperty

--- src/test/java/SelenideGitTests.java
+++ src/test/java/SelenideGitTests.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -8,12 +9,15 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class SelenideGitTests {
 
-    @Test
-    void findPageSoftAssertions () {
+    @BeforeAll
+    public static void configProperty () {
         Configuration.browserSize = System.getProperty("browserSize","1920x1080");
         Configuration.browser = System.getProperty("browser", "firefox");
         open("https://github.com/selenide/selenide");
-        
+    }
+
+    @Test
+    void findPageSoftAssertions () {
         $("#wiki-tab").click();
         $("#wiki-pages-filter").setValue("SoftAssertions");
         $(".filterable-active").shouldHave(text("SoftAssertions"));
